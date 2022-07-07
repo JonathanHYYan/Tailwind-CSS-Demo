e84504b196fe7b85e3a5f63d01b2042b84bd920e
jyan1234 <jonathan.yan10@googlemail.com>
2022-07-07 16:25:45 +0100

    final submission

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,22 @@ module.exports = {
     extend: {
       colors: {
         "heroGreen": "#BDD542",
+        "greyBG": "#EEEEEE",
+        "darkBlueTheme": "#00385E",
+        "greenTheme":"#BDD542"
       },
       "fontFamily":{
         body:["Mulish"]
       },
       gridTemplateColumns: {
         "60-40": "2fr 1fr",
-        "1-5": "1fr 5fr"
       },
+      keyframes: {
+        "mild-bounce": {
+          "0%, 100%": {transform:"translateY(-5%)"},
+          "50%": {transform:"translateY(0%)"},
+        }
+      }
     },
   },
   plugins: [],
